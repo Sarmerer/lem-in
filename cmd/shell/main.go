f3153ea51fce9e82fbf9c023974d605b63efc2d1
Ailen <ailensarmukhanova@gmail.com>
2020-07-27 13:05:14 +0500

    ants are moving now, example00 works

--- cmd/shell/main.go
+++ cmd/shell/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"lem-in/parser"
 	"lem-in/solver"
 )
 
 func main() {
 	//TO-DO: wtf is start and end in data????
-	_, graph := parser.ParseFile("example00.txt")
-	rooms := graph.GetRoomList()
-	paths := solver.EdmondsKarp(graph, rooms[0], rooms[len(rooms)-1])
-	fmt.Println("Paths: ", paths)
+	data, graph := parser.ParseFile("example00.txt")
+	ants, sink := solver.InitAntsAndAssignPaths(data.AntsAmount, graph)
+	solver.MoveAnts(ants, sink)
 }
